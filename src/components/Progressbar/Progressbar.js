7307e1c6b338ad4b6f5fd22570439aee64854bba
Gagan Deep <gagan.deep@digitas.com>
2022-08-11 01:39:25 +0530

    fixed active state in progress bar

--- src/components/Progressbar/Progressbar.js
+++ src/components/Progressbar/Progressbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Progressbar.css";
 
-const Progressbar = () => {
+const Progressbar = ({ step }) => {
   return (
     <ul id="progressbar">
       <li className="active"></li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <li className={`${step >= 2 ? `active` : ``}`}></li>
+      <li className={`${step >= 3 ? `active` : ``}`}></li>
+      <li className={`${step >= 4 ? `active` : ``}`}></li>
     </ul>
   );
 };
